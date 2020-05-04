79abc12cf64283342db7d4e67770a2f7569d50a0
LaisNSilva <silva.lais72@hotmial.com>
2020-05-04 18:39:20 -0300

    dicionario certo

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/DicionarioMorse.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/DicionarioMorse.java
@@ -3,6 +3,9 @@ package br.pro.hashi.ensino.desagil.projeto1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import java.util.LinkedList;
@@ -10,15 +13,42 @@ import java.util.LinkedList;
 public class DicionarioMorse extends AppCompatActivity {
 
     @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_dicionario_morse);
-            Translator tradutor = new Translator();
-            LinkedList<String> lista = new LinkedList<>();
-            lista = tradutor.getCodes();
-            for (int i = 0; i < (lista.size()); i++){
-                TextView alguma = findViewById(R.id.sera);
-            }
-        }
-}
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dicionario_morse);
+        Translator tradutor = new Translator();
+        LinkedList<String> lista_codigo = new LinkedList<>();
+        LinkedList<Character> lista_alfabeto = new LinkedList<>();
+        lista_codigo = tradutor.getCodes();
 
+        for (String codigo: lista_codigo){
+            Character t;
+            t = tradutor.morseToChar(codigo);
+            lista_alfabeto.add(t);
+        }
+
+
+
+        for (int i = 0; i < (lista_codigo.size()); i++){
+            TableLayout tableLayout =  (TableLayout) findViewById(R.id.tablelayout);
+            TableRow tablerow = new TableRow(this);
+            TextView textViewUm = new TextView(this);
+            TextView textViewDois = new TextView(this);
+            textViewUm.setTextSize(65);
+            textViewUm.setPadding(50,0,0,0);
+            textViewUm.setText(lista_codigo.get(i));
+            tablerow.addView(textViewUm);
+            textViewDois.setTextSize(65);
+            textViewDois.setPadding(150, 0, 0, 0);
+            textViewDois.setText(lista_alfabeto.get(i).toString());
+            tablerow.addView(textViewDois);
+            tableLayout.addView(tablerow);
+
+        }
+
+        
+
+
+    }
+
+}
